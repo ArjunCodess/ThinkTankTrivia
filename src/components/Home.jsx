15fda5c4219dd87c5d0dcf50757354b19a9e7a27
Arjun Vijay Prakash <137415649+ArjunCodess@users.noreply.github.com>
2024-01-03 20:44:33 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,9 @@ const Home = () => {
                 <h2 className='lg:text-4xl text-3xl font-medium text-[#333] lg:leading-normal leading-normal mb-3'>Master New Concepts <span className='text-blue-700 hover:text-blue-500 hover:tracking-widest transition-all duration-300'>Effortlessly</span></h2>
                 <p className='py-4 mb-6 text-gray-500 pl-5 border-l-4 border-blue-700 text-base'>We will guide you to success by ensuring that you are well prepared for exams and tests.</p>
                 <div className='text-lg font-medium block'>
-                    <Link to="/quiz"><button className='border-2 border-blue-700 bg-blue-700 px-6 py-2 text-white rounded-lg hover:tracking-widest transition-all duration-300'>View Catalog</button></Link>
-                    <Link to="/about"><button className='bg-white px-6 py-2 text-blue-700 border-2 border-blue-700 rounded-lg ml-3 hover:tracking-widest transition-all duration-300'><a href="/about"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block mr-1">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                        </svg>About Us</a></button></Link>
+                    <Link to="/quiz"><button className='border-2 border-blue-700 bg-blue-700 px-6 py-2 mb-3 text-white rounded-lg hover:tracking-widest mr-3 transition-all duration-300'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block mr-1">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>View Catalogue</button></Link>
                 </div>
             </div>
 
