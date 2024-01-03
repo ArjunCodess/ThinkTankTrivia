33fa6249f052920c6b03d95621b8123171d39e0b
Arjun Vijay Prakash <137415649+ArjunCodess@users.noreply.github.com>
2024-01-03 20:50:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
     <Navbar />
     <Outlet />
+    <Analytics />
     </>
   )
 }
